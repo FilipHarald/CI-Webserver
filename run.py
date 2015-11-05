c9161435f0ec46e8e0198ff7b8409bb21e2064de
FilipHarald <Filip.harald@gmail.com>
2015-11-05 14:44:19 +0100

    trying to change in run.py while server is running

--- run.py
+++ run.py
@@ -9,40 +9,20 @@ import bottle
 import subprocess
 import json
 
-
-'''---------------------------- Code for Routes with static files -----------------------------'''
-
-@route('/static/Css/<filename>')
-def send_static(filename):
-    return static_file(filename, root='./static/Css') 
-
-
-@route('/static/JS/<filename>')
-def send_static(filename):
-    return static_file(filename, root='./static/JS') 
-
-
-@route('/static/fonts/<filename>')
-def send_static(filename):
-    return static_file(filename, root='./static/Fonts') 
-
-
-@route('/static/images/<filename>')
-def send_static(filename):
-    return static_file(filename, root='./static/Images') 
-
-'''----------------------- Code for Routes --------------------------'''
-
+latest_author = ""
 
 @route('/', method='GET')
 def main_page():
     command = subprocess.call(['git', 'status'])
-    return template('index')
+    return template('index', latest_commit_by=latest_author)
 
 @route('/', method='POST')
 def main_page():
     print ('Git repo updated!! The following message was received:')
-    print (json.load(request.body))
+    payload_data = json.load(request.body)
+    print('Updating latest commit by...')
+    latestAuthor=payload_data.get('commits')[0].get('author').get('username')
+    print (payload_data)
     print('\nNow trying to pull the latest version...')
     status = subprocess.call(['git', 'pull'])
     print('\nFinished pulling, status code:')
@@ -54,3 +34,26 @@ def main_page():
 
 #command for running the server 'global'
 run(host='0.0.0.0', port=8080, debug=True, reloader=True)
+
+'''---------------------------- Code for Routes with static files -----------------------------'''
+
+@route('/static/Css/<filename>')
+def send_static(filename):
+    return static_file(filename, root='./static/Css')
+
+
+@route('/static/JS/<filename>')
+def send_static(filename):
+    return static_file(filename, root='./static/JS')
+
+
+@route('/static/fonts/<filename>')
+def send_static(filename):
+    return static_file(filename, root='./static/Fonts')
+
+
+@route('/static/images/<filename>')
+def send_static(filename):
+    return static_file(filename, root='./static/Images')
+
+'''----------------------- Code for Routes --------------------------'''
--- views/index.tpl
+++ views/index.tpl
@@ -8,6 +8,7 @@
             <h1>CI-webbserver</h1>
         </header>
        index.tpl
+       Latest commit made by: {{latestCommitBy}}
     </section>
 
 </body>
